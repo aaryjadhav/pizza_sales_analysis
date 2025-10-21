c9f6fcd1f65dd14fb29a3aac2572d6b952b4c57a
Aary Vijay Jadhav <aary.jadhav18@gmail.com>
2025-10-21 16:24:27 +0530

    query13

---
+++ pizza_sales/query_13.sql
@@ -0,0 +1,21 @@
+-- Determine the top 3 most ordered pizza types based on revenue for each pizza category. --
+SELECT category, name, revenue, rn
+FROM (
+    SELECT 
+        category,
+        name,
+        revenue,
+        RANK() OVER (PARTITION BY category ORDER BY revenue DESC) AS rn
+    FROM (
+        SELECT 
+            pizza_types.category AS category,
+            pizza_types.name AS name,
+            ROUND(SUM(order_details.quantity * pizzas.price), 0) AS revenue
+        FROM pizza_types
+        JOIN pizzas ON pizza_types.pizza_type_id = pizzas.pizza_type_id
+        JOIN order_details ON order_details.pizza_id = pizzas.pizza_id
+        GROUP BY category, name
+    ) AS subquery
+) AS ranked_pizzas
+WHERE rn <= 3
+ORDER BY category, rn;
