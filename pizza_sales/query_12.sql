eac6f710464bc2a0aa015cce4976dfd4cd8699d4
Aary Vijay Jadhav <aary.jadhav18@gmail.com>
2025-10-21 17:16:14 +0530

    Update query_12.sql

--- pizza_sales/query_12.sql
+++ pizza_sales/query_12.sql
@@ -1,15 +1,14 @@
 -- Analyze the cumulative revenue generated over time. --
-SELECT order_date,
-SUM(revenue) over(order by order_date) as cum_revenue
-from
-(SELECT 
-    orders.order_date,
-    ROUND(SUM(order_details.quantity * pizzas.price),
-            0) AS revenue
-FROM
-    order_details
-        JOIN
-    pizzas ON order_details.pizza_id = pizzas.pizza_id
-        JOIN
-    orders ON orders.order_id = order_details.order_id
-GROUP BY order_date) as sales;
+SELECT 
+    order_date,
+    SUM(revenue) OVER (ORDER BY order_date ROWS UNBOUNDED PRECEDING) AS cum_revenue
+FROM (
+    SELECT 
+        orders.order_date,
+        ROUND(SUM(order_details.quantity * pizzas.price), 0) AS revenue
+    FROM order_details
+    JOIN pizzas ON order_details.pizza_id = pizzas.pizza_id
+    JOIN orders ON orders.order_id = order_details.order_id
+    GROUP BY orders.order_date
+) AS sales
+ORDER BY order_date;
